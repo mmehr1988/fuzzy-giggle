16e1bb7b5f8ea15cc5cb45017c41b8a1fc222c86
Mehdi Mehrabani <mmehr1988@gmail.com>
2022-08-12 16:38:14 -0400

    added connection to mongoDB database

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 // Create express server
 const express = require('express');
+const colors = require('colors');
 
 // ======================================
 // PORT SETUP
@@ -10,11 +11,15 @@ require('dotenv').config();
 // Bring in graphql http from express-graphql package
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const connectDB = require('./config/db');
 // Create a port variable - it will first look in our environment variables for a port variables else the fallback is port 5000
 const port = process.env.PORT || 5000;
 
 // Create app variable to initialize express
 const app = express();
+
+// Connect to the database
+connectDB();
 
 // Create a USE
 // https://graphql.org/graphql-js/running-an-express-graphql-server/
