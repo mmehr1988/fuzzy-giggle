78364aeaf9b286eebe95453a4626dc4d986da61b
Mehdi Mehrabani <mmehr1988@gmail.com>
2022-08-24 15:24:56 -0400

    small update to server file

--- server/index.js
+++ server/index.js
@@ -38,9 +38,7 @@ app.use(
 );
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
